193dcc23179f07a4a87b35eb930b5aaab2c6af63
Apurba <apurba1907@gmail.com>
2025-01-10 15:13:41 +0530

    Project is working

---
+++ src/test/java/com/apurba/in/sample/TestSample.java
@@ -0,0 +1,18 @@
+package com.apurba.in.sample;
+
+import io.qameta.allure.Description;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestSample {
+    @Description("Sample TC 1")
+    @Test
+    public void testsample1(){
+        Assert.assertTrue(true);
+    }
+    @Description("Sample TC 1")
+    @Test
+    public void testsample2(){
+        Assert.assertTrue(true);
+    }
+}
